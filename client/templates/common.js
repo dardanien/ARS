dc5be3da9a042a12e3f56e85e8e5e213964e29bc
dardanien <gonzagues_jacques@yahoo.com>
2015-01-10 17:00:58 +0100

    test2

--- client/templates/common.js
+++ client/templates/common.js
@@ -1,12 +1,16 @@
 
-Template.detailForm.helpers ({
-  templateName: function() {
-    return Session.get("detailPageName");
-  },
+Template.detailTools.helpers ({
   editMode: function() {
       return Session.get("editMode");
   }
 });
+
+Template.detailForm.helpers ({
+  templateName: function() {
+    return Session.get("detailPageName");
+  }
+});
+
 
 Template.detailForm.events ({
   "click #addDataObject": function(event) {
