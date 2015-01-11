b3d09bf03b688dee5ad2063d622408e1556ff85f
dardanien <gonzagues_jacques@yahoo.com>
2015-01-11 23:30:43 +0000

    expander added

--- client/templates/expander.js
+++ client/templates/expander.js
@@ -1,13 +1,20 @@
-Template.detailForm.events ({
+                          
+Template.expander.events ({
   "click button": function(e) {
     e.preventDefault();
-    console.log("toto " + $("span"));
-    if ($("expander span").hasClass("glyphicon-collapse-up")) {
-      $("expander span").removeClass("glyphicon-collapse-up");
-      $("expander span").addClass("glyphicon-collapse-down");
+    // change the icon
+    var spanx= $(e.target).parent().find("span");
+
+    if (spanx.hasClass("glyphicon-collapse-up")) {
+      spanx.removeClass("glyphicon-collapse-up");
+      spanx.addClass("glyphicon-collapse-down");
     } else {
-      $("expander span").removeClass("glyphicon-collapse-down");
-      $("expander span").addClass("glyphicon-collapse-up");
+      spanx.removeClass("glyphicon-collapse-down");
+      spanx.addClass("glyphicon-collapse-up");
     }
+    // hide/show the sibbling form
+    var formx= $(e.target).parents("div").next(".collapse");
+    console.log("toto " + formx.length );
+    formx.toggle();
   }
 });
